f54b9fddf16fa9b7d14ffe75e3248bcf56a0e81f
SehajDhawan <sehaj.dhawan2962@gmail.com>
2023-07-19 12:51:02 +0530

    calculator_

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import {add,sub,mul,divi} from './Calculation'
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+      <li>The sum of 2 no. is {add(20,2)}</li>
+      <li>The sub of 2 no. is {sub(20,2)}</li>
+      <li>The mul of 2 no. is {mul(20,2)}</li>
+      <li>The div of 2 no. is {divi(20,3)}</li>
+    </ul>
+    </>
   );
 }
 
---
+++ src/Calculation.jsx
@@ -0,0 +1,22 @@
+function add(a,b){
+    let sum=a+b;
+    return sum;
+}
+
+function sub(a,b){
+    let sub=a-b;
+    return sub;
+}
+
+function mul(a,b){
+    let mul=a*b;
+    return mul;
+}
+
+function divi(a,b){
+    let divi=a/b;
+    divi=divi.toFixed(2);
+    return divi;
+}
+
+export {add,sub,mul,divi};
